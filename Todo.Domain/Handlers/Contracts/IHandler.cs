30d18f780883e0305b5b7eb401b6b418c391c83e
Matheus Rodrigues <Matheusrodrigues0296@gmail.com>
2020-02-19 15:57:15 -0300

    handler Todo create

--- Todo.Domain/Handlers/Contracts/IHandler.cs
+++ Todo.Domain/Handlers/Contracts/IHandler.cs
@@ -4,6 +4,6 @@ namespace Todo.Domain.Handlers.Contracts
 {
     public interface IHandler<T> where T : ICommand
     {
-         ICommandResult Handler(ICommand command);
+         ICommandResult Handler(T command);
     }
 }
--- Todo.Domain/Handlers/TodoHandler.cs
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -1,5 +1,7 @@
 using Flunt.Notifications;
+using Todo.Domain.Commands;
 using Todo.Domain.Commands.Contracts;
+using Todo.Domain.Entities;
 using Todo.Domain.Handlers.Contracts;
 using Todo.Domain.Repositories;
 
@@ -9,16 +11,31 @@ namespace Todo.Domain.Handlers
         Notifiable
       , IHandler<CreateTodoCommand>
     {
-        private readonly ITodoRepository  repository;
+        private readonly ITodoRepository  _repository;
 
         public TodoHandler(ITodoRepository repository)
         {
-            this.repository = repository;
+            _repository = repository;
         }
 
-        public ICommandResult Handler(ICommand command)
+        public ICommandResult Handler(CreateTodoCommand command)
         {
-            throw new System.NotImplementedException();
+            //Fail fast validation
+           command.Validate();
+           if(command.Invalid)
+                return new GenericCommandResult(false, "Ops, parece que sua terefa esta errada!", command.Notifications);
+
+            //Geara o Todo
+            var todo = new Todoitem(command.Title,  command.Date, command.User);
+
+            //salva no banco
+            _repository.Create(todo);
+
+            ///Retorna o resultado
+            return new GenericCommandResult(true, "Tarefa salva", todo.Id);
+                    
         }
+
+      
     }
 }
